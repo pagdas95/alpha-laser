56115432eab720d93393045c289f9c14b6703207
pagdas95 <pantelisa1995@gmail.com>
2025-11-15 02:18:44 +0200

    Remove sourcemap references from vendor bundles for production

--- alpha/static/libs/chart.js/chart.js
+++ alpha/static/libs/chart.js/chart.js
@@ -11469,4 +11469,3 @@ const registerables = [
 ];
 
 export { Animation, Animations, ArcElement, BarController, BarElement, BasePlatform, BasicPlatform, BubbleController, CategoryScale, Chart, plugin_colors as Colors, DatasetController, plugin_decimation as Decimation, DomPlatform, DoughnutController, Element, index as Filler, Interaction, plugin_legend as Legend, LineController, LineElement, LinearScale, LogarithmicScale, PieController, PointElement, PolarAreaController, RadarController, RadialLinearScale, Scale, ScatterController, plugin_subtitle as SubTitle, Ticks, TimeScale, TimeSeriesScale, plugin_title as Title, plugin_tooltip as Tooltip, adapters as _adapters, _detectPlatform, animator, controllers, defaults, elements, layouts, plugins, registerables, registry, scales };
-//# sourceMappingURL=chart.js.map
--- alpha/static/libs/leaflet/leaflet-src.js
+++ alpha/static/libs/leaflet/leaflet-src.js
@@ -14509,4 +14509,3 @@
   window.L = exports;
 
 }));
-//# sourceMappingURL=leaflet-src.js.map
